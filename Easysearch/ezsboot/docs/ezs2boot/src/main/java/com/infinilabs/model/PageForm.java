fa58937a020423d66a454ebeb5c4d04093855c37
zhanglei <zhanglei@infinilabs.com>
2024-04-26 15:34:10 +0800

    remove swagger

--- Easysearch/ezsboot/docs/ezs2boot/src/main/java/com/infinilabs/model/PageForm.java
+++ Easysearch/ezsboot/docs/ezs2boot/src/main/java/com/infinilabs/model/PageForm.java
@@ -1,27 +1,19 @@
 package com.infinilabs.model;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
-@Schema(title = "基础分页信息", name = "PageForm")
 public class PageForm {
 
-    @Schema(title = "页条数", example = "20")
     private int pageSize = 20;
 
-    @Schema(title = "第几页", example = "1")
     private int pageNum = 1;
 
-    @Schema(title = "排序字段: 可选: 不同列表排序不同需再协商", example = "desc")
     private String orderBy;
 
-    @Schema(title = "排序规则 true升序 false降序")
     private Boolean orderType = false;
 
-    @Schema(title = "排序游标")
     private Object[] sortCursor;
 
-    @Schema(title = "查询所有: 默认查询今日及所有未审核单子")
     private boolean isAll = false;
 }
--- Easysearch/ezsboot/docs/ezs2boot/src/test/java/com/infinilabs/EzsRHLCTests.java
+++ Easysearch/ezsboot/docs/ezs2boot/src/test/java/com/infinilabs/EzsRHLCTests.java
@@ -1,13 +1,23 @@
 package com.infinilabs;
 
 import com.alibaba.fastjson2.JSON;
+import com.infinilabs.utils.EzsUtil;
 import com.infinilabs.utils.LogUtil;
+import com.infinilabs.utils.StringUtil;
 import org.easysearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.easysearch.action.index.IndexRequest;
+import org.easysearch.action.index.IndexResponse;
+import org.easysearch.action.search.SearchRequest;
+import org.easysearch.action.search.SearchResponse;
 import org.easysearch.action.support.master.AcknowledgedResponse;
 import org.easysearch.client.RequestOptions;
 import org.easysearch.client.RestHighLevelClient;
 import org.easysearch.client.indices.CreateIndexRequest;
 import org.easysearch.client.indices.CreateIndexResponse;
+import org.easysearch.common.xcontent.XContentType;
+import org.easysearch.index.query.QueryBuilder;
+import org.easysearch.index.query.QueryBuilders;
+import org.easysearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -42,5 +52,27 @@ class EzsRHLCTests {
         AcknowledgedResponse response = client.indices().delete(request, RequestOptions.DEFAULT);
 
         LogUtil.formatInfo("Easysearch 索引删除结果：%s", JSON.toJSONString(response));
+    }
+
+    @Test
+    public void testIndexing() throws IOException {
+        String jsonStr = "{\"name\": \"张三\", \"address\": \"北京市\"}";
+        String indexName = "infinilabs_idx";
+        IndexRequest indexRequest = new IndexRequest(indexName);
+        // 不设置id 会自动使用es默认的
+        indexRequest.source(jsonStr, XContentType.JSON);
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testSearch() throws IOException {
+        String indexName = "infinilabs_idx";
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.matchAllQuery()).size(10);
+
+        sourceBuilder.fetchSource(new String[]{"name", "address"}, new String[]{});
+        SearchRequest request = new SearchRequest(indexName).source(sourceBuilder);
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
     }
 }
